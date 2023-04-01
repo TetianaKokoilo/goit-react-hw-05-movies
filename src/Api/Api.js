e0b809fcb53aaa7b5421a82a6608d7b708d26b33
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-04-02 00:44:47 +0300

    work with home

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,16 +1,55 @@
 import axios from 'axios';
 const API_KEY = '2458b78fc78c2faaef19c7dfe7e5ebeb';
-
 const API_TRENDING = 'https://api.themoviedb.org/3/trending/movie/day';
-// console.log(API_TRENDING);
+const API_MOVIE_ID = 'https://api.themoviedb.org/3/movie/';
+const API_SEARCH = 'https://api.themoviedb.org/3/search/movie';
 
-export const getTrandingMovies = async page => {
+export const getMovies = async (page,  total_pages) => {
   const { data } = await axios.get(API_TRENDING, {
     params: {
       api_key: API_KEY,
       page: page,
-      per_page: 10,
+      total_pages: total_pages,
     },
   });
   return data;
 };
+
+export const getMovieById = async id => {
+  const { data } = await axios.get(`${API_MOVIE_ID + String(id)}`, {
+    params: {
+      api_key: API_KEY,
+    },
+  });
+  return data;
+};
+
+export const getCredits = async id => {
+  const { data } = await axios.get(`${API_MOVIE_ID + String(id)}/credits`, {
+    params: {
+      api_key: API_KEY,
+    },
+  });
+  return data;
+};
+
+export const getReviews = async id => {
+  const { data } = await axios.get(`${API_MOVIE_ID + String(id)}/reviews`, {
+    params: {
+      api_key: API_KEY,
+    },
+  });
+  return data;
+};
+
+export const getKeyWord = async (searchQuery, page) => {
+  const {data } = await axios.get(API_SEARCH, {
+    params: {
+      api_key: API_KEY,
+      query: searchQuery,
+      page: page,
+    }
+  })
+
+  return data;
+}
